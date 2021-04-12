58d6e6cab29b51a833bec02337be974ff0749d1f
rbrewer76 <59892664+rbrewer76@users.noreply.github.com>
2021-04-12 15:15:31 -0600

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,28 @@
+
+import firebase from "firebase/app"
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+// Check if firebase is already initialized
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+const app = firebase.app()
+const auth = firebase.auth()
+const db = firebase.firestore()
+export {app, auth, db}
+
+console.log(app.name ? 'Firebase Connected' : 'Firebase not connected')
